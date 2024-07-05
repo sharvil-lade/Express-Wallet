e746a8adb818fc54d8d3a8d40e2715672be10048
sharvil-lade <sharvillade9917@gmail.com>
2024-07-05 23:33:09 +0530

    .

---
+++ backend/db.js
@@ -0,0 +1,77 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const { string } = require("zod");
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL);
+
+const historySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  sent: {
+    type: Boolean,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 30,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 30,
+  },
+  history: [historySchema],
+});
+
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+const Account = mongoose.model("Account", accountSchema);
+// const History = mongoose.model("History", historySchema);
+
+module.exports = {
+  User,
+  Account,
+  // History,
+};
